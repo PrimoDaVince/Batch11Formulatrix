592108870c0121529bb1badcffcc0f3637a514e8
Akmalzh <akmalzakyzainh@gmail.com>
2024-07-10 15:11:09 +0700

    Initial commit

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,13 +1,15 @@
-﻿using CalculatorLib;
+﻿//Menggunakan Lib Kalkulator yg sudah dibuat
+using CalculatorLib;
 class Program
 {
 	static void Main()
 	{
 		Calculator calculator = new Calculator();
 		//Memilih Menu 	
-		string menunote = "Welcome To Simple Calculator \n 1.Addition \n 2.Multipy \n 3.Divide \n 4.Subtraction";
-		Console.WriteLine(menunote + "\n Please Enter : ");
-
+		string welcomenotes = "Welcome To Simple Calculator ";
+		
+		Console.WriteLine(welcomenotes +  "\n Please Enter : ");
+		//fungsi untuk menggunakan kalkulator lagi
 		do
 		{
 			CalculatorStart(calculator);
@@ -17,7 +19,8 @@ class Program
 	}
 	static void CalculatorStart(Calculator calculator)
 	{
-
+		Console.WriteLine("\n 1.Addition \n 2.Multipy \n 3.Divide \n 4.Subtraction");
+		Console.WriteLine("\n Please Enter 1-4");
 		string chose = Console.ReadLine();
 
 		//Memassukan Angka Untuk Dihitung
@@ -28,28 +31,28 @@ class Program
 		//Convert String Menjadi INTEGER	
 		int convertin1 = int.Parse(input1);
 		int convertin2 = int.Parse(input2);
-		//Menentukan User	
+		//Menentukan Pilihan Model Perhitungan	
 		if (chose == "1")
 		{
 			int result = calculator.Add(convertin1, convertin2);
-			Console.WriteLine("Result " + convertin1 + " + " + convertin2 + result);
+			Console.WriteLine("Result " + convertin1 + " + " + convertin2 + " : " + result);
 		}
 		if (chose == "2")
 		{
 			int result = calculator.Multiply(convertin1, convertin2);
-			Console.WriteLine("Result " + convertin1 + " * " + convertin2 + result);
+			Console.WriteLine("Result " + convertin1 + " * " + convertin2 + " : " + result);
 		}
 		if (chose == "3")
 		{
 			int result = calculator.Divide(convertin1, convertin2);
-			Console.WriteLine("Result " + convertin1 + " / " + convertin2 + result);
+			Console.WriteLine("Result " + convertin1 + " / " + convertin2 + " : " + result);
 		}
 		if (chose == "4")
 		{
 			int result = calculator.Minus(convertin1, convertin2);
-			Console.WriteLine("Result " + convertin1 + " - " + convertin2 + result);
+			Console.WriteLine("Result " + convertin1 + " - " + convertin2 + " : " + result);
 		}
+		Console.WriteLine("Please Input 'Y' To continue");
 
-		Console.WriteLine("Need To Continue Calculator Please enter Y");
 	}
 }
---
+++ Project2/Program.cs
@@ -0,0 +1,38 @@
+﻿//constract overloading
+using System.Drawing;
+using System.Net.Sockets;
+
+class Phone
+{
+	public string screen;
+	public string soc;
+	public int size;
+	public int ram;
+	public int storage;
+	public Phone(string screen,
+				string soc,
+				int size,
+				int ram)
+	{
+	this.screen=screen;
+	this.soc=soc;
+	this.size=size;
+	this.ram=ram;	
+	Console.WriteLine($"Phone Created :{}");		
+	}
+	public Phone(int size,int ram)
+	{
+		this.size=size;
+		this.ram=ram;
+		Console.WriteLine("Phone Created");
+	}
+	
+	public Phone(string soc ,string screen)
+	{
+		this.soc=soc;
+		this.screen = screen;
+		Console.WriteLine("Phone Created");
+	}
+	
+	
+}
